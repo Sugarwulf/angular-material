ed26007d0263fb9546ab5a6b81cb99189dcf17de
Reggie Bingham <reggiebing11@gmail.com>
2017-05-04 15:48:12 -0700

    delete gitignore

---
+++ ngApp/app.js
@@ -0,0 +1,24 @@
+var ang_material;
+(function (ang_material) {
+    angular.module('ang_material', ['ui.router', 'ngResource', 'ui.bootstrap']).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider
+            .state('home', {
+            url: '/',
+            templateUrl: '/ngApp/views/home.html',
+            controller: ang_material.Controllers.HomeController,
+            controllerAs: 'controller'
+        })
+            .state('about', {
+            url: '/about',
+            templateUrl: '/ngApp/views/about.html',
+            controller: ang_material.Controllers.AboutController,
+            controllerAs: 'controller'
+        })
+            .state('notFound', {
+            url: '/notFound',
+            templateUrl: '/ngApp/views/notFound.html'
+        });
+        $urlRouterProvider.otherwise('/notFound');
+        $locationProvider.html5Mode(true);
+    });
+})(ang_material || (ang_material = {}));
---
+++ ngApp/controllers/controllers.js
@@ -0,0 +1,20 @@
+var ang_material;
+(function (ang_material) {
+    var Controllers;
+    (function (Controllers) {
+        var HomeController = (function () {
+            function HomeController() {
+                this.message = 'Hello from the home page!';
+            }
+            return HomeController;
+        }());
+        Controllers.HomeController = HomeController;
+        var AboutController = (function () {
+            function AboutController() {
+                this.message = 'Hello from the about page!';
+            }
+            return AboutController;
+        }());
+        Controllers.AboutController = AboutController;
+    })(Controllers = ang_material.Controllers || (ang_material.Controllers = {}));
+})(ang_material || (ang_material = {}));
